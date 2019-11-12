dcbe49c50e467367d41bdd316ddd6ff3f9b24e00
Kuczas <michalkuczkowski92@gmail.com>
2019-11-12 19:42:01 +0100

    pierwsza zmiana w projekcie

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+
+// sout ->System.out.println();
+// CTRl + D -> duplikowanie linii kodu
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Hello world!");
+        System.out.println("Publikujemy pierwszy program!");
     }
 }
